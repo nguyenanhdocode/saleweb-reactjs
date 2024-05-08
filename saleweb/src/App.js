592601b9daf19bb627c2bd64086a189861e50f92
nguyenanhdocode <nguyenanhdo200213@gmail.com>
2024-05-08 15:08:08 +0700

    Init project

---
+++ saleweb/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
